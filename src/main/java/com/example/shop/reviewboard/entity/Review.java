c3db968906acdb6350d28208066c9088b42b262e
PC <dns6574@naver.com>
2024-04-24 11:10:25 +0900

    commit

--- src/main/java/com/example/shop/reviewboard/entity/Review.java
+++ src/main/java/com/example/shop/reviewboard/entity/Review.java
@@ -4,12 +4,10 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
-import lombok.NoArgsConstructor;
 import lombok.RequiredArgsConstructor;
 
 @Entity
 @RequiredArgsConstructor
-@NoArgsConstructor
 public class Review {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
