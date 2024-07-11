62ca93094f2bffcbcd6ebb4ce948cb4ba4cd9381
PC <dns6574@naver.com>
2024-07-12 02:30:36 +0900

    commit

--- src/test/java/com/example/forum/ForumApplicationTests.java
+++ src/test/java/com/example/forum/ForumApplicationTests.java
@@ -1,12 +1,36 @@
 package com.example.forum;
 
+import com.example.forum.boards.freeBoard.board.entity.FreeBoard;
+import com.example.forum.boards.freeBoard.board.repository.FreeBoardRepository;
+import com.example.forum.user.auth.repository.UserAuthRepository;
+import com.example.forum.user.entity.User;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
+
+import java.time.LocalDateTime;
 
 @SpringBootTest
 class ForumApplicationTests {
-	@Test
-	public void test0() {
+	@Autowired
+	UserAuthRepository userAuthRepository;
 
+	@Autowired
+	FreeBoardRepository freeBoardRepository;
+
+	@Test
+	public void writePosts() {
+		User user =  userAuthRepository.findById(1L)
+				.orElseThrow(() -> new IllegalArgumentException("존재하지 않는 유저입니다"));
+		for(int i = 1; i < 50; i++){
+			FreeBoard freeBoard = FreeBoard.builder()
+					.title("테스트 " + String.valueOf(i))
+					.content("asdasd")
+					.createDate(LocalDateTime.now())
+					.user(user)
+					.view(0)
+					.build();
+			freeBoardRepository.save(freeBoard);
+		}
 	}
 }
